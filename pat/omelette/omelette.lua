7b7db0a5004cd4f81eb02f03d619a1b6f406f8fb
patman <27874300+patmanf@users.noreply.github.com>
2021-02-22 11:27:00 -0600

    make an omelette

---
+++ pat/omelette/omelette.lua
@@ -0,0 +1,21 @@
+local omelette_init = init or function() end
+
+function init()
+	omelette_init()
+	
+	local list = player.collectables("cooking")
+	local omelette = false
+	
+	for _,v in ipairs(list) do
+		if v == "omelette" then
+			omelette = true
+			break
+		end
+	end
+	
+	if not omelette then
+		player.interact("ScriptPane", "/pat/omelette/omelette.config")
+	else
+		player.interact("ScriptPane", "/pat/omelette/omelette2.config")
+	end
+end
